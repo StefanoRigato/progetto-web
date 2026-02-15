82c1732b974e73ffe111c5697fac28d9137b99c9
StefanoRigato <stefanorigato7@gmail.com>
2026-02-15 22:57:23 +0100

    Removed old function position

--- src/utility/utility.js
+++ src/utility/utility.js
@@ -1,21 +1,3 @@
 import Lotus from "../assets/img/lotus.png";
 
 export const defaultImage = (onErrorEvent) =>  onErrorEvent.target.src = Lotus;
-
-/*
-function filterWeaponsByNameObj(weaponsObj, search) {
-    const q = search.trim();
-    if (!q) return weaponsObj;
-
-    const re = new RegExp(escapeRegExp(q), "i");
-
-    return Object.fromEntries(
-        Object.entries(weaponsObj).filter(([, w]) => re.test(w?.name ?? ""))
-    );
-}
-
-export const queryWeapon = (weapon, query) => {
-    //const escapeRegExp = (s) => s.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // escape user input
-    return Object.values(filterWeaponsByNameObj(weapon, query));
-}
-    */
